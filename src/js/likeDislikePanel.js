ddbf133a0137e7218935b9a1220f821f9c66cd72
RobinH <Robin.Hawiz.6670@student.uu.se>
2025-02-22 23:45:17 +0100

    js: Implemented my own scrool feature (still working on it though)

--- src/js/likeDislikePanel.js
+++ src/js/likeDislikePanel.js
@@ -1,3 +1,6 @@
+const { Root } = require("postcss");
+
+const header = document.querySelector("header");
 const likeDislikePanel = document.querySelector(".like-dislike-panel");
 const bio = document.querySelector(".bio");
 const bioArticles = document.querySelectorAll(".bio article");
@@ -7,7 +10,20 @@ toggleBioButton.addEventListener("click", () => {
   rotateToggleBioButton();
   displayBio();
   repositionPanel();
-  scrollToElem();
+  let bioDOMRect = bio.getBoundingClientRect();
+  let bioDistanceToTop = window.scrollY + bioDOMRect.top;
+  // Determines initial scroll speed. The smaller the value, the greater the speed.
+  // If true, value will be set for the desktop version of the webb app, otherwise set value for the mobile version.
+  let stepDenominator =
+    window.innerWidth > 534
+      ? bioDistanceToTop / 37.3
+      : bioDistanceToTop / 27 - 7000 / window.innerHeight;
+  // Determines how quickly the scroll speed tapers off. The smaller the value, the faster it slows down.
+  // If true, value will be set for the desktop version of the webb app, otherwise set value for the mobile version.
+  let diffDenominator =
+    window.innerWidth > 534 ? bioDistanceToTop / 86.9 : bioDistanceToTop / 86.9;
+
+  scrollToElem(bioDOMRect, stepDenominator, diffDenominator, bioDistanceToTop);
 });
 
 function rotateToggleBioButton() {
@@ -34,21 +50,51 @@ function showBio() {
 }
 
 function hideBio() {
-  setTimeout(() => (bio.style.display = "none"), 710);
+  setTimeout(() => (bio.style.display = "none"), 730);
 }
 
 function repositionPanel() {
   if (toggleBioButton.classList.contains("opened")) {
     likeDislikePanel.style.bottom = `${bio.offsetHeight}px`;
   } else {
-    setTimeout(() => (likeDislikePanel.style.bottom = 0), 710);
+    setTimeout(() => (likeDislikePanel.style.bottom = 0), 730);
   }
 }
 
-function scrollToElem() {
+function scrollToElem(
+  bioDOMRect,
+  stepDenominator,
+  diffDenominator,
+  bioDistanceToTop
+) {
+  let isScrolledToBottom =
+    window.scrollY + window.innerHeight >= document.body.scrollHeight;
+  let rootScrollTopVal =
+    document.documentElement.scrollTop || document.body.scrollTop;
+  // Scroll to the bio element that opened
   if (toggleBioButton.classList.contains("opened")) {
-    bio.scrollIntoView({ behavior: "smooth" });
-  } else {
-    document.body.scrollIntoView({ behavior: "smooth" });
+    if (bioDOMRect.top - rootScrollTopVal > 0 && !isScrolledToBottom) {
+      let yCoordinate = Math.ceil(
+        rootScrollTopVal + bioDistanceToTop / stepDenominator
+      );
+      bioDistanceToTop -= bioDistanceToTop / diffDenominator;
+      window.scrollTo(0, yCoordinate);
+
+      window.requestAnimationFrame(() => {
+        scrollToElem(
+          bioDOMRect,
+          stepDenominator,
+          diffDenominator,
+          bioDistanceToTop
+        );
+      });
+    }
+  }
+  // Scroll to the top of the page
+  else {
+    if (rootScrollTopVal > 0) {
+      window.scrollTo(0, rootScrollTopVal - rootScrollTopVal / 9);
+      window.requestAnimationFrame(scrollToElem);
+    }
   }
 }
