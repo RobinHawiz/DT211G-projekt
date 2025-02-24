c1cd14a42209115effbc0f0c26b144871cf3dca1
RobinH <Robin.Hawiz.6670@student.uu.se>
2025-02-24 18:11:19 +0100

    js: Usage of local storage added to prevent too many calls being made to apis

--- src/js/cardGenerator.js
+++ src/js/cardGenerator.js
@@ -1,16 +1,22 @@
 const { fetchData } = require("./modules/fetchData");
 const { convertDogData } = require("./modules/convertData");
 async function generateCard() {
-  //Get data
-  let dogBreedsData = [await fetchData("https://dogapi.dog/api/v2/breeds")];
-  let tempDogBreedsData = dogBreedsData[0];
-  while (tempDogBreedsData.links.next != undefined) {
-    tempDogBreedsData = await fetchData(tempDogBreedsData.links.next);
-    dogBreedsData.push(tempDogBreedsData);
+  let completeDogData;
+  if (localStorage.getItem("dogData") === null) {
+    //Get data
+    let dogBreedsData = [await fetchData("https://dogapi.dog/api/v2/breeds")];
+    let tempDogBreedsData = dogBreedsData[0];
+    while (tempDogBreedsData.links.next != undefined) {
+      tempDogBreedsData = await fetchData(tempDogBreedsData.links.next);
+      dogBreedsData.push(tempDogBreedsData);
+    }
+    const dogGroupsData = await fetchData("https://dogapi.dog/api/v2/groups");
+    //Convert data
+    completeDogData = await convertDogData(dogBreedsData, dogGroupsData);
+    localStorage.setItem("dogData", JSON.stringify(completeDogData));
+  } else {
+    completeDogData = JSON.parse(localStorage.getItem("dogData"));
   }
-  const dogGroupsData = await fetchData("https://dogapi.dog/api/v2/groups");
-  //Convert data
-  const completeDogData = await convertDogData(dogBreedsData, dogGroupsData);
   console.log("completeDogData:");
   console.log(completeDogData);
 }
