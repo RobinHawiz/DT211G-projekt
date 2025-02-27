67ebc6ee26ce7511528c1a937a3b472312fe11ab
RobinH <Robin.Hawiz.6670@student.uu.se>
2025-02-27 21:27:46 +0100

    feat: Big update, you can now swipe to like and dislike each card (only with mouse, touch for mobile will be implemented later)

--- src/js/likeDislikePanel.js
+++ src/js/likeDislikePanel.js
@@ -2,35 +2,222 @@ const { getDogData, generateInitialCard } = require("./modules/cardGenerator");
 const { generateCard } = require("./modules/cardGenerator");
 const { dogData } = require("./findMatch");
 
-const likeDislikePanel = document.querySelector(".like-dislike-panel");
-let bio = document.querySelector(".bio");
+const like = document.querySelector(".like");
+const dislike = document.querySelector(".dislike");
+const toggleSettings = document.querySelector(".toggle-settings");
+let card = document.querySelector(".card.current");
+let bio = document.querySelector(".bio.current");
+let cardStylingLeft = 0;
+let cardStylingTop = 0;
+let cardRotationDeg = 0;
+let isBeingLiked = false;
+let isBeingDisliked = false;
+let likeOrDislikeButtonWasClicked = false;
 let bioArticles = document.querySelectorAll(".bio article");
 const bioButton = document.querySelector("button.toggle-bio");
-const likeButton = document.querySelector("button.like");
+let newX = 0,
+  newY = 0,
+  startX = 0,
+  startY = 0;
 
-likeButton.addEventListener("click", async () => {
+card.addEventListener("mousedown", mouseDown);
+
+function mouseDown(e) {
+  like.classList.toggle("scale-down");
+  dislike.classList.toggle("scale-down");
+  toggleSettings.classList.toggle("scale-down");
+  bioButton.classList.toggle("scale-down");
+
+  startX = e.clientX;
+  startY = e.clientY;
+
+  document.addEventListener("mousemove", mouseMove);
+  document.addEventListener("mouseup", moveCard);
+}
+
+function mouseMove(e) {
+  newX = startX - e.clientX;
+  newY = startY - e.clientY;
+
+  startX = e.clientX;
+  startY = e.clientY;
+
+  cardStylingLeft = card.offsetLeft - newX;
+  cardStylingTop = card.offsetTop - newY;
+
+  cardCenterX = card.getBoundingClientRect().left + card.offsetWidth / 2;
+  windowCenterX = window.innerWidth / 2;
+  cardRotationDeg = (windowCenterX - cardCenterX) / 30;
+
+  card.style.left = card.offsetLeft - newX + "px";
+  card.style.top = card.offsetTop - newY + "px";
+  card.style.transform = `rotate(${cardRotationDeg}deg)`;
+  // Dog is being disliked.
+  if (windowCenterX - cardCenterX > 200) {
+    isBeingDisliked = true;
+    if (dislike.classList.contains("scale-down"))
+      dislike.classList.toggle("scale-down");
+  }
+
+  // Dog is being liked.
+  else if (windowCenterX - cardCenterX < -100) {
+    isBeingLiked = true;
+    if (like.classList.contains("scale-down"))
+      like.classList.toggle("scale-down");
+  }
+  // Dog is being neither liked nor disliked.
+  else {
+    isBeingLiked = false;
+    isBeingDisliked = false;
+    if (!dislike.classList.contains("scale-down"))
+      dislike.classList.toggle("scale-down");
+    if (!like.classList.contains("scale-down"))
+      like.classList.toggle("scale-down");
+  }
+}
+
+function moveCard() {
+  document.removeEventListener("mousemove", mouseMove);
+  document.removeEventListener("mouseup", moveCard);
+  if (like.classList.contains("scale-down"))
+    like.classList.toggle("scale-down");
+  if (dislike.classList.contains("scale-down"))
+    dislike.classList.toggle("scale-down");
+  if (toggleSettings.classList.contains("scale-down"))
+    toggleSettings.classList.toggle("scale-down");
+  if (bioButton.classList.toggle("scale-down"))
+    bioButton.classList.toggle("scale-down");
+  let duration = 900; // Milliseconds.
+  let currentX = cardStylingLeft;
+  let currentY = cardStylingTop;
+  let distanceX;
+  let distanceY;
+  let currentRotationDeg = cardRotationDeg;
+  let distanceRotationDeg;
+  let startTime = null;
+  const card = document.querySelector(".card.current");
+  if (isBeingLiked) {
+    distanceX =
+      window.innerWidth / 2 + card.offsetWidth / 2 - currentX / 2 + 200;
+    distanceRotationDeg = likeOrDislikeButtonWasClicked ? 30 : 0;
+    window.requestAnimationFrame(moveCardOutOfView);
+    resetCardValues();
+    initNewCard();
+    card.addEventListener("mousedown", mouseDown);
+  } else if (isBeingDisliked) {
+    distanceX =
+      -window.innerWidth / 2 - card.offsetWidth / 2 - currentX / 2 - 200;
+    console.log(likeOrDislikeButtonWasClicked);
+    distanceRotationDeg = likeOrDislikeButtonWasClicked ? -30 : 0;
+    window.requestAnimationFrame(moveCardOutOfView);
+    resetCardValues();
+    initNewCard();
+    card.addEventListener("mousedown", mouseDown);
+  } else {
+    duration = 700;
+    distanceX = cardStylingLeft * -1;
+    distanceY = cardStylingTop * -1;
+    distanceRotationDeg = cardRotationDeg * -1;
+    window.requestAnimationFrame(moveCardToInitialPos);
+  }
+  function moveCardOutOfView(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    const elapsedTime = currentTime - startTime;
+    const leftPos = easeOutQuint(elapsedTime, currentX, distanceX, duration);
+    const rotDeg = easeOutQuintCard(
+      elapsedTime,
+      currentRotationDeg,
+      distanceRotationDeg,
+      duration
+    );
+    card.style.left = leftPos + "px";
+    card.style.transform = `rotate(${rotDeg}deg)`;
+    if (elapsedTime < duration)
+      animationFrameId = window.requestAnimationFrame(moveCardOutOfView);
+  }
+  function moveCardToInitialPos(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    const elapsedTime = currentTime - startTime;
+    const leftPos = easeOutBack(elapsedTime, currentX, distanceX, duration);
+    const topPos = easeOutBack(elapsedTime, currentY, distanceY, duration);
+    const rotDeg = easeOutQuintCard(
+      elapsedTime,
+      currentRotationDeg,
+      distanceRotationDeg,
+      duration
+    );
+    card.style.left = leftPos + "px";
+    card.style.top = topPos + "px";
+    card.style.transform = `rotate(${rotDeg}deg)`;
+    if (elapsedTime < duration)
+      window.requestAnimationFrame(moveCardToInitialPos);
+    else {
+      resetCardValues();
+      card.addEventListener("mousedown", mouseDown);
+    }
+  }
+}
+
+async function initNewCard() {
   // Remove the front card.
-  document.querySelector(".card").remove();
-  document.querySelector(".bio").remove();
-  // Move the card that was behind the front card forward.
-  document.querySelector(".card").classList.remove("behind");
-  document.querySelector(".bio").classList.remove("behind");
+  card.classList.remove("current");
+  bio.classList.remove("current");
+  card.classList.add("being-removed");
+  bio.classList.add("being-removed");
 
-  // Hook up the new bio.
-  bio = document.querySelector(".bio");
-  bioArticles = document.querySelectorAll(".bio article");
+  setTimeout(() => {
+    document.querySelector(".card.being-removed").remove();
+    document.querySelector(".bio.being-removed").remove();
+  }, 700);
+
+  // Move the card that was behind the front card forward.
+  let cardBehind = document.querySelector(".card.behind");
+  let bioBehind = document.querySelector(".bio.behind");
+  cardBehind.classList.remove("behind");
+  bioBehind.classList.remove("behind");
+  cardBehind.classList.add("current");
+  bioBehind.classList.add("current");
+
+  // Hook up the new card and bio.
+  card = cardBehind;
+  bio = bioBehind;
+  bioArticles = document.querySelectorAll(".bio.current article");
+  card.addEventListener("mousedown", mouseDown);
 
   // Generate a new card that gets put behind the front card.
   await generateCard(dogData[0]);
   dogData.splice(0, 1);
-});
+}
 
 bioButton.addEventListener("click", () => {
   rotateBioButton();
   displayBio();
-  repositionPanel();
+  changeCardStylingPosition();
   scrollPage();
 });
+
+like.addEventListener("click", () => {
+  isBeingLiked = true;
+  isBeingDisliked = false;
+  likeOrDislikeButtonWasClicked = true;
+  moveCard();
+});
+
+dislike.addEventListener("click", () => {
+  isBeingDisliked = true;
+  isBeingLiked = false;
+  likeOrDislikeButtonWasClicked = true;
+  moveCard();
+});
+
+function resetCardValues() {
+  cardStylingLeft = 0;
+  cardStylingTop = 0;
+  cardRotationDeg = 0;
+  isBeingLiked = false;
+  isBeingDisliked = false;
+  likeOrDislikeButtonWasClicked = false;
+}
 
 function rotateBioButton() {
   // true => open animation, false => close animation
@@ -59,11 +246,11 @@ function hideBio() {
   setTimeout(() => (bio.style.display = "none"), 730);
 }
 
-function repositionPanel() {
+function changeCardStylingPosition() {
   if (bioButton.classList.contains("opened")) {
-    likeDislikePanel.style.bottom = `${bio.offsetHeight}px`;
+    card.style.position = "relative";
   } else {
-    setTimeout(() => (likeDislikePanel.style.bottom = 0), 730);
+    setTimeout(() => (card.style.position = "absolute"), 730);
   }
 }
 
@@ -122,4 +309,13 @@ d = Duration - Amount of time the animation will take.
 */
 function easeOutQuint(t, b, c, d) {
   return c * ((t = t / d - 1) * t * t * t * t + 1) + b + 2;
+}
+
+function easeOutQuintCard(t, b, c, d) {
+  return c * ((t = t / d - 1) * t * t * t * t + 1) + b;
+}
+
+function easeOutBack(t, b, c, d) {
+  let s = 1.2;
+  return c * ((t = t / d - 1) * t * ((s + 1) * t + s) + 1) + b;
 }
