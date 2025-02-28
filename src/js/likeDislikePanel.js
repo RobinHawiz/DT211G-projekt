8ba4f903bdfcbe3ee63f2d460cabafe5fd13f460
RobinH <Robin.Hawiz.6670@student.uu.se>
2025-02-28 16:34:47 +0100

    js: Swipe now works on mobile devices too

--- src/js/likeDislikePanel.js
+++ src/js/likeDislikePanel.js
@@ -13,6 +13,7 @@ let cardRotationDeg = 0;
 let isBeingLiked = false;
 let isBeingDisliked = false;
 let likeOrDislikeButtonWasClicked = false;
+let isMobileDevice = userDeviceIsMobile();
 let bioArticles = document.querySelectorAll(".bio article");
 const bioButton = document.querySelector("button.toggle-bio");
 let newX = 0,
@@ -20,27 +21,35 @@ let newX = 0,
   startX = 0,
   startY = 0;
 
-card.addEventListener("mousedown", mouseDown);
+isMobileDevice
+  ? card.addEventListener("touchstart", mouseDownOrTouchStart)
+  : card.addEventListener("mousedown", mouseDownOrTouchStart);
 
-function mouseDown(e) {
+function mouseDownOrTouchStart(e) {
+  e.preventDefault(); // (For mobile) Disables: zooming with two fingers, pull down to refreshing the page, click events, etc.
   like.classList.toggle("scale-down");
   dislike.classList.toggle("scale-down");
   toggleSettings.classList.toggle("scale-down");
   bioButton.classList.toggle("scale-down");
 
-  startX = e.clientX;
-  startY = e.clientY;
+  startX = isMobileDevice ? e.targetTouches[0].clientX : e.clientX;
+  startY = isMobileDevice ? e.targetTouches[0].clientY : e.clientY;
 
-  document.addEventListener("mousemove", mouseMove);
-  document.addEventListener("mouseup", moveCard);
+  if (isMobileDevice) {
+    document.addEventListener("touchmove", mouseMoveOrTouchMove);
+    document.addEventListener("touchend", moveCard);
+  } else {
+    document.addEventListener("mousemove", mouseMoveOrTouchMove);
+    document.addEventListener("mouseup", moveCard);
+  }
 }
 
-function mouseMove(e) {
-  newX = startX - e.clientX;
-  newY = startY - e.clientY;
+function mouseMoveOrTouchMove(e) {
+  newX = startX - (isMobileDevice ? e.targetTouches[0].clientX : e.clientX);
+  newY = startY - (isMobileDevice ? e.targetTouches[0].clientY : e.clientY);
 
-  startX = e.clientX;
-  startY = e.clientY;
+  startX = isMobileDevice ? e.targetTouches[0].clientX : e.clientX;
+  startY = isMobileDevice ? e.targetTouches[0].clientY : e.clientY;
 
   cardStylingLeft = card.offsetLeft - newX;
   cardStylingTop = card.offsetTop - newY;
@@ -52,6 +61,7 @@ function mouseMove(e) {
   card.style.left = card.offsetLeft - newX + "px";
   card.style.top = card.offsetTop - newY + "px";
   card.style.transform = `rotate(${cardRotationDeg}deg)`;
+
   // Dog is being disliked.
   if (windowCenterX - cardCenterX > 200) {
     isBeingDisliked = true;
@@ -77,11 +87,17 @@ function mouseMove(e) {
 }
 
 function moveCard() {
-  document
-    .querySelector(".card.current")
-    .removeEventListener("mousedown", mouseDown);
-  document.removeEventListener("mousemove", mouseMove);
-  document.removeEventListener("mouseup", moveCard);
+  let cardBeingMoved = document.querySelector(".card.current");
+  if (isMobileDevice) {
+    cardBeingMoved.removeEventListener("touchstart", mouseDownOrTouchStart);
+    document.removeEventListener("touchmove", mouseMoveOrTouchMove);
+    document.removeEventListener("touchend", moveCard);
+  } else {
+    cardBeingMoved.removeEventListener("mousedown", mouseDownOrTouchStart);
+    document.removeEventListener("mousemove", mouseMoveOrTouchMove);
+    document.removeEventListener("mouseup", moveCard);
+  }
+
   if (like.classList.contains("scale-down"))
     like.classList.toggle("scale-down");
   if (dislike.classList.contains("scale-down"))
@@ -109,7 +125,6 @@ function moveCard() {
   } else if (isBeingDisliked) {
     distanceX =
       -window.innerWidth / 2 - card.offsetWidth / 2 - currentX / 2 - 200;
-    console.log(likeOrDislikeButtonWasClicked);
     distanceRotationDeg = likeOrDislikeButtonWasClicked ? -30 : 0;
     window.requestAnimationFrame(moveCardOutOfView);
     resetCardValues();
@@ -154,7 +169,9 @@ function moveCard() {
       window.requestAnimationFrame(moveCardToInitialPos);
     else {
       resetCardValues();
-      card.addEventListener("mousedown", mouseDown);
+      isMobileDevice
+        ? card.addEventListener("touchstart", mouseDownOrTouchStart)
+        : card.addEventListener("mousedown", mouseDownOrTouchStart);
     }
   }
 }
@@ -183,7 +200,9 @@ async function initNewCard() {
   card = cardBehind;
   bio = bioBehind;
   bioArticles = document.querySelectorAll(".bio.current article");
-  card.addEventListener("mousedown", mouseDown);
+  isMobileDevice
+    ? card.addEventListener("touchstart", mouseDownOrTouchStart)
+    : card.addEventListener("mousedown", mouseDownOrTouchStart);
 
   // Generate a new card that gets put behind the front card.
   await generateCard(dogData[0]);
@@ -197,6 +216,7 @@ function toggleBio() {
   rotateBioButton();
   displayBio(); // This function call toggles the class "opened" on the bioButton.
   disableSwipe();
+  removeHtmlOverflowHidden();
   changeCardStylingPosition();
   scrollPage();
   setTimeout(() => {
@@ -252,12 +272,21 @@ function rotateBioButton() {
 }
 
 function disableSwipe() {
-  card.removeEventListener("mousedown", mouseDown);
+  isMobileDevice
+    ? card.removeEventListener("touchstart", mouseDownOrTouchStart)
+    : card.removeEventListener("mousedown", mouseDownOrTouchStart);
 }
 
 function enableSwipe() {
-  if (!bioButton.classList.contains("opened"))
-    card.addEventListener("mousedown", mouseDown);
+  if (!bioButton.classList.contains("opened")) {
+    isMobileDevice
+      ? card.addEventListener("touchstart", mouseDownOrTouchStart)
+      : card.addEventListener("mousedown", mouseDownOrTouchStart);
+  }
+}
+
+function removeHtmlOverflowHidden() {
+  document.querySelector("html").style.overflow = "visible";
 }
 
 function displayBio() {
@@ -348,4 +377,22 @@ function easeOutQuintCard(t, b, c, d) {
 function easeOutBack(t, b, c, d) {
   let s = 1.2;
   return c * ((t = t / d - 1) * t * ((s + 1) * t + s) + 1) + b;
+}
+
+function userDeviceIsMobile() {
+  let check = false;
+  // Got this from http://detectmobilebrowsers.com/
+  (function (a) {
+    if (
+      /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+        a
+      ) ||
+      /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+        a.substr(0, 4)
+      )
+    ) {
+      check = true;
+    }
+  })(navigator.userAgent || navigator.vendor || window.operax);
+  return check;
 }
