28133feb254df6287a01132197b9962996ae64a6
RobinH <Robin.Hawiz.6670@student.uu.se>
2025-02-28 18:58:58 +0100

    Optimization: Every newly generated card requested new icons. Now they don't.

--- src/js/modules/cardGenerator.js
+++ src/js/modules/cardGenerator.js
@@ -31,86 +31,39 @@ export async function getDogData() {
 export async function generateCard(dogData) {
   const cardWrapper = document.querySelector(".card-wrapper");
 
-  const card = document.createElement("div");
-  card.classList.add("card");
-  card.classList.add("behind");
-  card.innerHTML = `
-            <div class="img-wrapper">
-            <img
-              class="like-icon"
-              src="${likeIcon}"
-              alt="Like icon"
-            />
-            <img
-              class="dislike-icon"
-              src="${dislikeIcon}"
-              alt="Dislike icon"
-            />
-            <img
-              class="dog-img"
-              src="${dogData.img}"
-              alt="A ${dogData.name}"
-            />
-          </div>
-          <div class="dog-name-and-group">
-            <h2>${dogData.name}</h2>
-            <h3>${dogData.group}</h3>
-          </div>
-`;
+  const cardBehind = document.querySelector(".card.current").cloneNode(true);
+  cardBehind.classList.remove("current");
+  cardBehind.classList.add("behind");
 
-  const bio = document.createElement("div");
-  bio.classList.add("bio");
-  bio.classList.add("behind");
-  bio.innerHTML = `
-            <article class="description">
-            <div class="content">
-              <div class="title">
-                <img src="${infoIcon}" alt="Info icon" />
-                <h3>Bio</h3>
-              </div>
-              <p>
-              ${dogData.description}
-              </p>
-            </div>
-          </article>
-          <article class="Attributes">
-            <div class="content">
-              <div class="title">
-                <img src="${dogIcon}" alt="Dog icon" />
-                <h3>Attributes</h3>
-              </div>
-          <div class="attribute">
-            <img src="${cakeIcon}" alt="Cake icon" />
-            <p>Life expectancy: ${dogData.lifeExpectancy[0]}-${
-    dogData.lifeExpectancy[1]
-  } years</p>
-          </div>
-          <span class="divider"></span>
-          <div class="attribute">
-            <img src="${scaleIcon}" alt="Scale icon" />
-            <div class="male-female-weights">
-              <p>Male weight: ${dogData.maleWeight[0]}-${
-    dogData.maleWeight[1]
-  }kg</p>
-              <p>Female weight: ${dogData.femaleWeight[0]}-${
-    dogData.femaleWeight[1]
-  }kg</p>
-            </div>
-          </div>
-              <span class="divider"></span>
-          <div class="attribute">
-            <img src="${bacteriumIcon}" alt="Bacterium icon" />
-            <p>Hypoallergenic: ${dogData.hypoallergenic ? `Yes` : `No`}</p>
-          </div>
-            </div>
-          </article>
-  `;
+  cardBehind.querySelector(".dog-img").src = dogData.img;
+  cardBehind.querySelector(".dog-img").alt = "A " + dogData.name;
+  cardBehind.querySelector(".dog-name-and-group h2").innerText = dogData.name;
+  cardBehind.querySelector(".dog-name-and-group h3").innerText = dogData.group;
+
+  const bioBehind = document.querySelector(".bio.current").cloneNode(true);
+  bioBehind.classList.remove("current");
+  bioBehind.classList.add("behind");
+
+  bioBehind.querySelector(".description .content p").innerText =
+    dogData.description;
+  bioBehind.querySelector(
+    ".Attributes .attribute.life-expectancy p"
+  ).innerText = `Life expectancy: ${dogData.lifeExpectancy[0]}-${dogData.lifeExpectancy[1]} years`;
+  bioBehind.querySelector(
+    ".Attributes .attribute.weight .male-weight"
+  ).innerText = `${dogData.maleWeight[0]}-${dogData.maleWeight[1]}kg`;
+  bioBehind.querySelector(
+    ".Attributes .attribute.weight .female-weight"
+  ).innerText = `${dogData.femaleWeight[0]}-${dogData.femaleWeight[1]}kg`;
+  bioBehind.querySelector(
+    ".Attributes .attribute.hypoallergenic p"
+  ).innerText = `Hypoallergenic: ${dogData.hypoallergenic ? `Yes` : `No`}`;
 
   cardWrapper.insertBefore(
-    card,
+    cardBehind,
     cardWrapper.querySelector(".like-dislike-panel")
   );
-  card.parentNode.insertBefore(bio, card.nextSibling);
+  cardBehind.parentNode.insertBefore(bioBehind, cardBehind.nextSibling);
 }
 
 // The first card has to be generated this way because we need the card element and bio element to already exist in our HTML.
@@ -164,26 +117,26 @@ export async function generateInitialCard(dogData) {
             <img src="${dogIcon}" alt="Dog icon" />
             <h3>Attributes</h3>
           </div>
-          <div class="attribute">
+          <div class="attribute life-expectancy">
             <img src="${cakeIcon}" alt="Cake icon" />
             <p>Life expectancy: ${dogData.lifeExpectancy[0]}-${
     dogData.lifeExpectancy[1]
   } years</p>
           </div>
           <span class="divider"></span>
-          <div class="attribute">
+          <div class="attribute weight">
             <img src="${scaleIcon}" alt="Scale icon" />
             <div class="male-female-weights">
-              <p>Male weight: ${dogData.maleWeight[0]}-${
+              <p class="male-weight">Male weight: ${dogData.maleWeight[0]}-${
     dogData.maleWeight[1]
   }kg</p>
-              <p>Female weight: ${dogData.femaleWeight[0]}-${
-    dogData.femaleWeight[1]
-  }kg</p>
+              <p class="female-weight">Female weight: ${
+                dogData.femaleWeight[0]
+              }-${dogData.femaleWeight[1]}kg</p>
             </div>
           </div>
           <span class="divider"></span>
-          <div class="attribute">
+          <div class="attribute hypoallergenic">
             <img src="${bacteriumIcon}" alt="Bacterium icon" />
             <p>Hypoallergenic: ${dogData.hypoallergenic ? `Yes` : `No`}</p>
           </div>
