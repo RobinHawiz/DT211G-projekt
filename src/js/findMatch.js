a1e6e7ad631de7b02741deda892ed76ae7d1cc8e
RobinH <Robin.Hawiz.6670@student.uu.se>
2025-03-04 12:01:00 +0100

    refactor: Removing redundant function findMatchApp

--- src/js/findMatch.js
+++ src/js/findMatch.js
@@ -2,16 +2,12 @@ const { getDogData, generateInitialCard } = require("./modules/cardGenerator");
 const { generateCard } = require("./modules/cardGenerator");
 export let dogData;
 
-async function findMatchApp() {
-  await init();
-  require("./likeDislikePanel");
-}
-
 async function init() {
   dogData = await getDogData();
   await generateInitialCard(dogData[0]);
   await generateCard(dogData[1]);
   dogData.splice(0, 2);
+  require("./likeDislikePanel");
 }
 
-findMatchApp();
+init();
