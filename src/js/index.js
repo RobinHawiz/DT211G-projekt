0f7856b075c4543a61dcf4bd8cde1c755e1c81f7
RobinH <Robin.Hawiz.6670@student.uu.se>
2025-03-05 14:34:09 +0100

    doc: Added comments in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,9 @@
+/**
+ * @file Handles the header and doggyBurger animation.
+ * @requires module:headerAnimation
+ * @requires doggyburger
+ */
+
 import "../style/style.scss";
 const {
   logoAnimation,
@@ -5,9 +11,18 @@ const {
   doggyBurgerLinesAnimation,
 } = require("./modules/headerAnimation");
 require("./doggyburger");
+
+/*
+ * Get the current URL path.
+ */
 var path = window.location.pathname;
 var page = path.split("/").slice(-2)[0];
 
+/**
+ * Initializes header animations based on the current page.
+ * If the user is on the "DT211G-projekt" page, animations start with higher intensity.
+ * Otherwise, animations are initialized with lower intensity.
+ */
 if (page === "DT211G-projekt") {
   logoAnimation(1);
   logoImgAnimation(1);
