26c7c8d1a6b7fa2144a1e8b9b37bca10ab10fd26
RobinH <Robin.Hawiz.6670@student.uu.se>
2025-03-05 19:20:06 +0100

    doc: Added comments in headerAnimation.js

--- src/js/modules/headerAnimation.js
+++ src/js/modules/headerAnimation.js
@@ -3,6 +3,12 @@ const logoImg = document.querySelector(".logo .logo-img");
 const letters = logo.textContent.split("");
 const doggyBurgerLines = document.querySelectorAll(".doggy-burger span");
 
+/**
+ * Splits the logo text into individual letters and animates them with a delay.
+ * Each letter is wrapped in a span and given a CSS animation to animate its appearance.
+ *
+ * @param {number} delay - The delay (in seconds) before starting the animation for each letter.
+ */
 export function logoAnimation(delay) {
   logo.innerHTML = "";
   for (let i = 0; i <= letters.length - 1; i++) {
@@ -13,10 +19,20 @@ export function logoAnimation(delay) {
   }
 }
 
+/**
+ * Animates the logo image with a CSS animation, using the specified delay.
+ *
+ * @param {number} delay - The delay (in seconds) before starting the animation on the logo image.
+ */
 export function logoImgAnimation(delay) {
   logoImg.style.animation = `0.5s ease ${delay}s forwards logo-img-animation`;
 }
 
+/**
+ * Animates the lines of the "doggy burger" menu icon with individual CSS animations for each span line.
+ *
+ * @param {number} delay - The delay (in seconds) before starting the animation for each doggyBurger span line.
+ */
 export function doggyBurgerLinesAnimation(delay) {
   doggyBurgerLines[0].style.animation = `0.5s ease ${delay}s forwards hamburger-span-1-animation`;
   doggyBurgerLines[1].style.animation = `0.5s ease ${delay}s forwards hamburger-span-2-animation`;
